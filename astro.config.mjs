729a13676996ce847a74482e6ae029e7f5aa5d2c
ymc0182 <501414437@qq.com>
2024-06-18 14:28:28 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [tailwind()]
 });
